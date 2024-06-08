b2f41bf99763cfade520233b239c7a8add2eb436
Garret Jennings <gjennings@me.com>
2024-06-08 09:44:38 -0700

    chore: update API version

--- PvPJ/main.lua
+++ PvPJ/main.lua
@@ -4,7 +4,7 @@ local addon = {
 	displayNameShort = "|cFF3DA5PvPJ|r",
 	author = "Scootworks (Updated by g4rr3t)",
 	version = "27",
-	api = {101041, nil},
+	api = {101042, nil},
 	savedVarVersion = "1",
 }
 
